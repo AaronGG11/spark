a07cfccd9b04b89073e05b5cd62b6af66bb05f44
Aaron Garcia <aarongarcia.ipn.escom@gmail.com>
2022-02-27 23:47:56 -0600

    Example 1 scala

---
+++ scala/regex.scala
@@ -0,0 +1,32 @@
+def funcion(contenido: String) : String = {
+    val REGEX_1 = "[^0-9]"
+    val REGEX_2 = "[^A-Z^]"
+    val REGEX_3 = "[^a-z]"
+
+    var at1 = List("Aaron","jose","pedro","arturo")
+    var at2 = List("maria","Juana","fernanda")
+    var at3 = List("ariel")
+
+    var resultado = {
+        if(at1.contains(contenido)) contenido.replaceAll(REGEX_1, "") 
+        else if(at2.contains(contenido)) contenido.replaceAll(REGEX_2, "") 
+        else contenido.replaceAll(REGEX_3, "")
+    }
+
+    resultado
+}
+
+print(funcion("Juana"))
+
+
+
+    val REGEX_1 = "[^0-9]"
+    val REGEX_2 = "[^A-Z^]"
+    val REGEX_3 = "[^a-z]"
+
+    var at1 = List("aaron","jose","pedro","arturo")
+    var at2 = List("maria","juana","fernanda")
+    var at3 = List("ariel")
+
+
+    print(at1.con)
