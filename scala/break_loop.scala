e5c71f408b857e52e0ac35eb94fe3b3419afa85c
Aaron Garcia <aarongarcia.ipn.escom@gmail.com>
2022-03-02 23:23:03 -0600

    Siguientes pruebas

---
+++ scala/break_loop.scala
@@ -0,0 +1,24 @@
+import scala.util.control.Breaks._
+object MainObject 
+{
+      
+// Main method
+def main(args: Array[String]) 
+{
+      
+    // Here, breakable is used to prevent exception 
+    breakable 
+    { 
+        for (a <- 1 to 10) 
+        {
+            if (a == 6)
+          
+                // terminate the loop when 
+                // the value of a is equal to 6
+                break 
+            else
+             println(a);
+        }
+    }
+}
+}
--- scala/regex.scala
+++ scala/regex.scala
@@ -1,19 +1,53 @@
-def funcion(contenido: String) : String = {
-    val REGEX_1 = "[^0-9]"
-    val REGEX_2 = "[^A-Z^]"
-    val REGEX_3 = "[^a-z]"
+import java.util
+import scala.util.control.Breaks._
 
-    var at1 = List("Aaron","jose","pedro","arturo")
-    var at2 = List("maria","Juana","fernanda")
-    var at3 = List("ariel")
+object MainObject { 
+    // Main method
+    def main(args: Array[String]) {
 
-    var resultado = {
-        if(at1.contains(contenido)) contenido.replaceAll(REGEX_1, "") 
-        else if(at2.contains(contenido)) contenido.replaceAll(REGEX_2, "") 
-        else contenido.replaceAll(REGEX_3, "")
+        val REGEX_1 = "[^0-9]"
+        val REGEX_2 = "[^A-Z]"
+        val REGEX_3 = "[^a-z]"
+
+
+        var dato = "Juana Morales"
+        var result = ""
+
+
+        val arlista= new util.ArrayList[List[String]]()
+        arlista.add(List("Aaro d vcfv54n","jo4se","p3dr0","3rt8ro","1w233a654a"))
+        arlista.add(List("maria","Juana Morales","fernanda"))
+        arlista.add(List("ariel","01 e3scd cfv 5t5653 rgrebrtb 4"))
+
+
+        val regex = new util.ArrayList[String]()
+        regex.add(REGEX_1)
+        regex.add(REGEX_2)
+        regex.add(REGEX_3)
+
+
+        println("---------- SALIDA ----------")
+
+        breakable{
+            for( a <- 0 to arlista.size()-1){
+            println("Entre " + a)
+            if(arlista.get(a).contains(dato)){
+                println("Deteneniedno ... " + a)
+                result = dato.replaceAll(regex.get(a), "") 
+                break
+            }
+        }
+        }
+
+
+        println(result)
+        println(regex.get(0))
+        println(regex.get(1))
+        println(regex.get(2))
+
+
+
+        println("---------- SALIDA ----------")
+
     }
-
-    resultado
 }
-
-print(funcion("Juana"))
